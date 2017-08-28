f1c1dd2c0219c5af51a5e87d71b2684a969d3ec0
mpedrolli <mpedrolli@users.noreply.github.com>
2017-08-28 16:58:45 +0200

    Journal/Topic names are with space safe

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # logbook
 A simple and UNIX-style logbook for nerds.
+
+## todo
+
+
+
--- logbook.sh
+++ logbook.sh
@@ -9,20 +9,18 @@ if [ ! -d $LOGBOOK_DIR ] ; then
 fi
 
 if [[ -z "$1" ]]; then
-	DATE=$(date +"%Y%m%d")
+	LOGBOOK_DATE=$(date +"%Y%m%d")
 else
-	DATE=$1
+	LOGBOOK_DATE=$1
 fi
 
-echo -n "Please enter the topic of this journal: "
-read TOPIC
+IFS= read -r -p "Enter the topic of this journal: " LOGBOOK_TOPIC
 
-LOGBOOK_FILE_NAME=$DATE-$TOPIC.$LOGBOOK_FILE_EXTENSION
+LOGBOOK_FILE_NAME=$LOGBOOK_DATE-${LOGBOOK_TOPIC// /-}.$LOGBOOK_FILE_EXTENSION
 
-
-echo "==============================" > $LOGBOOK_DIR/$LOGBOOK_FILE_NAME
-echo "$DATE - $TOPIC" >> $LOGBOOK_DIR/$LOGBOOK_FILE_NAME
-echo "==============================" >> $LOGBOOK_DIR/$LOGBOOK_FILE_NAME
+echo "===============================" > $LOGBOOK_DIR/$LOGBOOK_FILE_NAME
+echo "$LOGBOOK_DATE - "$LOGBOOK_TOPIC"" >> $LOGBOOK_DIR/$LOGBOOK_FILE_NAME
+echo "===============================" >> $LOGBOOK_DIR/$LOGBOOK_FILE_NAME
 
 $LOGBOOK_EDITOR $LOGBOOK_DIR/$LOGBOOK_FILE_NAME
 
